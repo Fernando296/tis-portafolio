ef91b6d0a3f2fa2dabf844d03295d1b6f1c0463a
Fabiola Huayna <fabiolahuaynach@gmail.com>
2026-03-27 13:33:12 -0400

    Uso de la api, registro de usuario

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/profile/basic-info', [ProfileController::class, 'show']);
+Route::put('/profile/basic-info', [ProfileController::class, 'updateBasicInfo']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [AuthController::class, 'register']);
