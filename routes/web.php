f8eef0c151a3df39885dda21af177d4cefa06f3e
Fabiola Huayna <fabiolahuaynach@gmail.com>
2026-03-27 13:34:25 -0400

    Ruta del empoint

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/perfil', function () {
+    return view('profile');
+});
+Route::get('/Registrarse', function () {
+    return view('Registro');
+});
