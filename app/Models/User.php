ab893a959768372ee36f66a2a58972cb0b573733
Mayra <choquemaqueramayra67@gmail.com>
2026-03-28 18:31:48 -0400

    correciones para que funcione RegistroInformacionbasica

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,44 +2,32 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    /*  MUY IMPORTANTE */
+    protected $table = 'usuarios';
+
+    protected $primaryKey = 'id_usuario';
+
+    public $timestamps = false; // solo si tu tabla no tiene created_at y updated_at
+
     protected $fillable = [
-        'name',
+        'nombre',
+        'apellido',
         'email',
         'password',
+        'profesion',
+        'biografia'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,23 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/profile/basic-info', [ProfileController::class, 'show']);
-Route::put('/profile/basic-info', [ProfileController::class, 'updateBasicInfo']);
-
+/* AUTH */
 Route::post('/register', [AuthController::class, 'register']);
-
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/*  TODAS LAS RUTAS PROTEGIDAS POR TOKEN */
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    /* PERFIL */
+    Route::get('/profile/basic-info', [ProfileController::class, 'show']);
+    Route::put('/profile/basic-info', [ProfileController::class, 'update']);
 });
